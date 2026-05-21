2961da14693bf110a4dfefd345fcba94da28da7a
systemhaibhai03-cloud <systemhaibhai03@gmail.com>
2026-05-21 13:59:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   base: '/my-project/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
